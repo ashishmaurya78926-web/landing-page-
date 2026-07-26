749021c6ca506c55badcd4ad1eb155fd867fe181
ashishmaurya78926-web <ashishmaurya78926@gmail.com>
2026-07-26 11:21:30 +0530

    Add files via upload

---
+++ DOC-20260721-WA0006..js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const applyButton = document.getElementById('applyBtn');
+
+    
+
+    applyButton.addEventListener('click', () => {
+
+        alert('Thank you for your interest! The application form is loading...');
+
+    });
+
+});
+
